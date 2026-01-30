b317fb356ea1cde168350492e695a64e93e78b61
Mazdak Nasab <mazdak.nasab@gmail.com>
2026-01-29 19:53:48 -0800

    Start a CLI

--- cmd/fwsim.go
+++ cmd/fwsim.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+
+	"github.com/mazdakn/fwsim/pkg/engine"
+	"github.com/sirupsen/logrus"
+)
+
+const (
+	defaultInputFile = "rules.yaml"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	input := flag.String("i", defaultInputFile, "input file with all rules and packets")
+	flag.Parse()
+
+	if input == nil || len(*input) == 0 {
+		logrus.Errorf("No input file")
+		os.Exit(1)
+	}
+
+	e := engine.New(*input)
+	e.Validate()
 }
--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,1 +1,15 @@
 package engine
+
+type Engine struct {
+	input string
+}
+
+func New(input string) *Engine {
+	return &Engine{
+		input: input,
+	}
+}
+
+func (e *Engine) Validate() {
+
+}
