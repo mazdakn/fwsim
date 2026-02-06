b47d43d7da803bfefc3972dc2c53b62d5ca67374
Mazdak Nasab <mazdak.nasab@gmail.com>
2026-02-05 18:59:02 -0800

    store rules in engine

--- pkg/engine/engine.go
+++ pkg/engine/engine.go
@@ -1,12 +1,17 @@
 package engine
 
+import "github.com/mazdakn/fwsim/pkg/policy"
+
 type Engine struct {
 	input string
+
+	store *policy.Store
 }
 
 func New(input string) *Engine {
 	return &Engine{
 		input: input,
+		store: policy.NewStore(),
 	}
 }
 
